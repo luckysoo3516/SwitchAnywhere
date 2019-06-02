5798946277926dac925e9e98ee5d3909cd623aff
luckysoo3516 <sumin328030@gmail.com>
2019-06-02 17:08:05 +0900

    Create Home

---
+++ web-client/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img v-if="isOn" class="bulb-image" src="../assets/bulbOn.jpg">
+    <img v-else class="bulb-image" src="../assets/bulbOff.jpg">
+    <button :disabled="isOn" @click="isOn=true">켜기</button>
+    <button :disabled="!isOn" @click="isOn=false">끄기</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isOn: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .bulb-image {
+    width: 300px;
+    height: 300px;
+  }
+</style>
