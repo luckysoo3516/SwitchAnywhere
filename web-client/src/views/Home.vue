adf35e1a9f2dce3411cfd8fd4a189bde6ddf4aee
luckysoo3516 <sumin328030@gmail.com>
2019-06-03 21:32:03 +0900

    import webiopi.js

--- web-client/src/views/Home.vue
+++ web-client/src/views/Home.vue
@@ -3,15 +3,33 @@
     <img v-if="isOn" class="bulb-image" src="../assets/bulbOn.jpg">
     <img v-else class="bulb-image" src="../assets/bulbOff.jpg">
     <button :disabled="isOn" @click="isOn=true">켜기</button>
-    <button :disabled="!isOn" @click="isOn=false">끄기</button>
+    <button :disabled="!isOn" @click="test()">끄기</button>
   </div>
 </template>
 
 <script>
+import 'expose-loader?$!expose-loader?jQuery!jquery'
+import WebIOPi from '../webiopi.js'
+
 export default {
   data () {
     return {
       isOn: true
+    }
+  },
+  methods: {
+    test() {
+      let w = new WebIOPi()
+      w.ready(function() {
+        w.callMacro("go_forward");
+      });
+
+    },
+    test2() {
+          let w = new WebIOPi()
+          w.ready(function() {
+            w.callMacro("go_backward");
+          });
     }
   }
 }
